9e1da77a14f3ac24e3191681ca54fa3280b1d7d5
paulafranca <paulatavaresfranca@gmail.com>
2015-03-19 14:30:50 -0300

    Primeiro Commit

---
+++ Ativ_02/02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+float conc(a, b, c){
+      float m;
+      m = (a + b + c)/3;
+      if(m <= 3){
+           printf("Reprovado\n");
+           }
+      else if(m > 3 && m <= 7){
+           printf("Exame\n");
+           
+      else
